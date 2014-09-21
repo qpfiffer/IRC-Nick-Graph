074c838fef3616291d438a642a1b17552862b44d
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-09-21 13:58:14 -0700

    stuff

--- nick_normalize.cpp
+++ nick_normalize.cpp
@@ -1,30 +1,36 @@
-#include <cstdio>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include "nick_normalize.h"
 
-#include <unordered_map>
-#include <unordered_set>
+StringToInt read_line(const char *buf, const unsigned int offset) {
+    // Read until a null or newline char
+    std::string to_return;
+    int read = 0;
+    while (true) {
+        char c = '\0';
+        c = buf[read + offset];
+        to_return.append(&c);
+        read++;
+        if (c == '\0' || c == '\n')
+            break;
+    }
 
-class Edge;
+    return std::make_tuple(to_return, read);
+}
 
-class Node {
-    private:
-        std::unordered_set<Edge> edges;
-};
+Graph *parse(const char *mmapd_log_file, const size_t length) {
+    unsigned int i = 0;
+    unsigned int total_read = 0;
+    Graph *king = new Graph();
 
-class Edge {
-    private:
-        std::unordered_set<Node> nodes;
-};
+    while (total_read < length) {
+        StringToInt line = read_line(mmapd_log_file, total_read);
+        total_read += std::get<1>(line);
 
-class Graph {
-    private:
-        std::unordered_set<Node> nodes;
-};
-
-int parse(const char *mmapd_log_file) {
+        std::string *line_str = &std::get<0>(line);
+        if (line_str->find(" has joined ") != std::string::npos ||
+            line_str->find(" is now known as ") != std::string::npos) {
+            printf("Got a hit with: %s", line_str->c_str());
+        }
+    }
 
     return 0;
 }
@@ -47,10 +53,11 @@ int main(int argc, char *argv[]) {
     }
 
     void *mmapd_log_file = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, log_file, 0);
-    parse((const char *)mmapd_log_file);
+    Graph *king = parse((const char *)mmapd_log_file, sb.st_size);
 
     munmap(mmapd_log_file, sb.st_size);
     close(log_file);
+    delete king;
 
     return 0;
 }
