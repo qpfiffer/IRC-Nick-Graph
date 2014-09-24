a40a8295f0fec5809d5588ca0d996c4532d5bfce
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-09-23 19:59:38 -0700

    Does this help? Maybe. I don't know.

--- nick_normalize.cpp
+++ nick_normalize.cpp
@@ -114,15 +114,17 @@ void Graph::printAliases() {
 bool FuckNamespaces::EdgeEqualTo::operator()(const Edge *x, const Edge *y) const {
     std::stringstream lhs;
     lhs << *x;
+    std::string lhs_string(lhs.str());
 
     std::stringstream rhs;
     rhs << *y;
+    std::string rhs_string(rhs.str());
 
-    const char *rhs_str, *lhs_str;
-    lhs_str = lhs.str().c_str();
-    rhs_str = rhs.str().c_str();
+    const char *lhs_str = lhs_string.c_str();
+    const char *rhs_str = rhs_string.c_str();
 
-    return lhs.str() == rhs.str();
+    bool result = lhs_string == rhs_string;
+    return result;
 }
 
 std::ostream& FuckNamespaces::operator<<(std::ostream& os, const Edge& edge) {
